3c87e536aa9eb611ec1fb4a45e501e5eb5776fbf
mykolak <mykola.kolisnyk@thetrainline.com>
2017-07-03 16:28:21 +0100

    Sample tests and Gradle wrapper were added

---
+++ src/test/java/com/sample/tests/junit/SampleTest.java
@@ -0,0 +1,12 @@
+package com.sample.tests.junit;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+
+	@Test
+	public void testSample() {
+		
+	}
+}
---
+++ src/test/java/com/sample/tests/testng/SampleTestNGTest.java
@@ -0,0 +1,11 @@
+package com.sample.tests.testng;
+
+import org.testng.annotations.Test;
+
+public class SampleTestNGTest {
+
+	@Test
+	public void testSample() {
+		
+	}
+}
