34c2d7ed0a57278b9cc75876fbfac8e21ab61bbb
mykolak <mykola.kolisnyk@thetrainline.com>
2017-07-17 17:48:41 +0100

    Data-driven tests were created

--- src/test/java/com/sample/tests/junit/SampleTest.java
+++ src/test/java/com/sample/tests/junit/SampleTest.java
@@ -4,10 +4,16 @@ import io.appium.java_client.android.AndroidDriver;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,10 +23,19 @@ import org.testng.Assert;
 
 import com.sample.framework.Configuration;
 
+@RunWith(Parameterized.class)
 public class SampleTest {
 
 	private WebDriver driver;
 
+	private String destination;
+	private boolean isBusiness;
+	
+	public SampleTest(String destination, boolean isBusiness) {
+		super();
+		this.destination = destination;
+		this.isBusiness = isBusiness;
+	}
 	@Before
 	public void setUp() throws Exception {
 		Configuration.load();
@@ -37,24 +52,37 @@ public class SampleTest {
         cap.setCapability("appWaitPackage", Configuration.get("appPackage"));
         cap.setCapability("fullReset", true);
         driver = new AndroidDriver<WebElement>(new URL(Configuration.get("driver_url")), cap);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	@After
 	public void tearDown() {
 		driver.quit();
 	}
 	
+	@Parameters
+    public static Collection<Object[]> getParameters() {
+        return Arrays.asList(
+            new Object[][] {
+            		{"London", true},
+            		{"Manchester", false},
+            });
+    }
+	
 	@Test
-	public void testSample() {
+	public void testSample() throws Exception {
 		driver.findElement(By.id("com.booking:id/btn_start_search")).click();
 		driver.findElement(By.id("com.booking:id/search_searchInput")).click();
-		driver.findElement(By.id("com.booking:id/disam_search")).sendKeys("Manchester");
+		driver.findElement(By.id("com.booking:id/disam_search")).sendKeys(this.destination);
+		Thread.sleep(3000);
 		driver.findElements(By.id("com.booking:id/disam_list_root")).get(0).click();
 		driver.findElement(By.xpath("(//android.widget.TextView[contains(@resource-id, 'calendar_tv') and @enabled='true'])[1]")).click();
-		driver.findElement(By.id("com.booking:id/business_purpose_business")).click();
+		if (this.isBusiness) {
+			driver.findElement(By.id("com.booking:id/business_purpose_business")).click();
+		} else {
+			driver.findElement(By.id("com.booking:id/business_purpose_leisure")).click();
+		}
 		driver.findElement(By.id("com.booking:id/search_search")).click();
 		String actualTitle = driver.findElement(By.id("com.booking:id/subtitle_layout_text")).getText();
-		Assert.assertEquals(actualTitle, "Manchester");
-		
-		
+		Assert.assertEquals(actualTitle, this.destination);
 	}
 }
--- src/test/java/com/sample/tests/testng/SampleTestNGTest.java
+++ src/test/java/com/sample/tests/testng/SampleTestNGTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 
 import java.io.File;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,8 +12,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.sample.framework.Configuration;
@@ -21,7 +23,7 @@ public class SampleTestNGTest {
 
 	private WebDriver driver;
 
-	@BeforeTest
+	@BeforeMethod(alwaysRun = true)
 	public void setUp() throws Exception {
 		Configuration.load();
         DesiredCapabilities cap = new DesiredCapabilities();
@@ -36,23 +38,37 @@ public class SampleTestNGTest {
         cap.setCapability("appWaitActivity", Configuration.get("appActivity"));
         cap.setCapability("appWaitPackage", Configuration.get("appPackage"));
         cap.setCapability("fullReset", true);
-        driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        driver = new AndroidDriver<WebElement>(new URL(Configuration.get("driver_url")), cap);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
-	@AfterTest
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		driver.quit();
 	}
+
+	@DataProvider(name = "source")
+	public static Object[][] getParameters() {
+        return new Object[][] {
+        		{"London", true },
+        		{"Manchester", false},
+        };
+    }
 	
-	@Test
-	public void testSample() {
+	@Test(dataProvider="source")
+	public void testSample(String destination, boolean isBusiness) throws Exception {
 		driver.findElement(By.id("com.booking:id/btn_start_search")).click();
 		driver.findElement(By.id("com.booking:id/search_searchInput")).click();
-		driver.findElement(By.id("com.booking:id/disam_search")).sendKeys("Manchester");
+		driver.findElement(By.id("com.booking:id/disam_search")).sendKeys(destination);
+		Thread.sleep(3000);
 		driver.findElements(By.id("com.booking:id/disam_list_root")).get(0).click();
 		driver.findElement(By.xpath("(//android.widget.TextView[contains(@resource-id, 'calendar_tv') and @enabled='true'])[1]")).click();
-		driver.findElement(By.id("com.booking:id/business_purpose_business")).click();
+		if (isBusiness) {
+			driver.findElement(By.id("com.booking:id/business_purpose_business")).click();
+		} else {
+			driver.findElement(By.id("com.booking:id/business_purpose_leisure")).click();
+		}
 		driver.findElement(By.id("com.booking:id/search_search")).click();
 		String actualTitle = driver.findElement(By.id("com.booking:id/subtitle_layout_text")).getText();
-		Assert.assertEquals(actualTitle, "Manchester");
+		Assert.assertEquals(actualTitle, destination);
 	}
 }
