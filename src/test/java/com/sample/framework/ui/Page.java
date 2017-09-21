ee062e4afcc2eb83145ef45095ae71b2214a5722
mykolak <mykola.kolisnyk@thetrainline.com>
2017-09-21 17:31:48 +0100

    Cucumber test was added

--- src/test/java/com/sample/framework/ui/Page.java
+++ src/test/java/com/sample/framework/ui/Page.java
@@ -6,6 +6,8 @@ import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -14,6 +16,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.Augmenter;
+import org.reflections.Reflections;
 
 import com.sample.framework.Configuration;
 import com.sample.framework.Driver;
@@ -25,12 +28,33 @@ public class Page {
     private static final int SCROLL_TOP_PART = 9;
     private static final int SCROLL_TOTAL_PARTS = 10;
     private static final long TIMEOUT = Configuration.timeout();
+    private static ConcurrentHashMap<String, Page> currentPages = new ConcurrentHashMap<String, Page>();
+
     private WebDriver driver;
 
     public Page(WebDriver driverValue) {
         this.driver = driverValue;
     }
-
+    public static Page screen(String name) throws Exception {
+        return screen(name, Configuration.get("pages_package"));
+    }
+    public static Page screen(String name, String pagePackage) throws Exception {
+        Reflections reflections = new Reflections(pagePackage);
+        Set<Class<? extends Page>> subTypes = reflections.getSubTypesOf(Page.class);
+        for (Class<? extends Page> type : subTypes) {
+            Alias annotation = type.getAnnotation(Alias.class);
+            if (annotation != null && annotation.value().equals(name)) {
+                return PageFactory.init(Driver.current(), type);
+            }
+        }
+        return null;
+    }
+    public static Page getCurrent() {
+        return currentPages.get(Driver.getThreadName());
+    }
+    public static void setCurrent(Page newPage) {
+        currentPages.put(Driver.getThreadName(), newPage);
+    }
     public WebDriver getDriver() {
         return driver;
     }
@@ -261,5 +285,16 @@ public class Page {
     }
     public boolean anyOfElementsIsDisabled(Control[] elements) throws Exception {
         return anyOfElementsIs(elements, "disabled");
+    }
+    public Control onPage(String name) throws Exception {
+        for (Field field : this.getClass().getFields()) {
+            if (Control.class.isAssignableFrom(field.getType())) {
+                Alias alias = field.getAnnotation(Alias.class);
+                if (alias != null && name.equals(alias.value())) {
+                    return (Control) field.get(this);
+                }
+            }
+        }
+        return null;
     }
 }
--- src/test/java/com/sample/tests/pages/SearchResultsPage.java
+++ src/test/java/com/sample/tests/pages/SearchResultsPage.java
@@ -2,13 +2,16 @@ package com.sample.tests.pages;
 
 import org.openqa.selenium.WebDriver;
 
+import com.sample.framework.ui.Alias;
 import com.sample.framework.ui.FindBy;
 import com.sample.framework.ui.Page;
 import com.sample.framework.ui.ScrollTo;
 import com.sample.framework.ui.controls.Control;
 
+@Alias("Search Results")
 public class SearchResultsPage extends Page {
 
+    @Alias("Title")
 	@FindBy(locator = "com.booking:id/subtitle_layout_text")
 	public Control textSubTitle;
 	@FindBy(locator = "//*[contains(@text, 'Park Plaza')]",
